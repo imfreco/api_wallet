4ac9c54e6349b436cef816eadeda5c82e2728ae9
fredycorts7 <fred.cor.14@gmail.com>
2020-12-04 18:26:32 -0500

    <ADD> agregando servicios  y controladores de prueba utilizando el patrón de inyección de dependencias con awilix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.APP_ENV = process.env.APP_ENV || 'development';
+
+import dotenv = require('dotenv');
+
+dotenv.config({
+  path: `${__dirname}/../config/${process.env.APP_ENV}.env`,
+});
+
+console.log(process.env.APP_FOO);
+
 import express = require('express');
+import { loadControllers } from 'awilix-express';
+import loadContainer from './container';
+
 const app: express.Application = express();
 
-app.use('/', (req, res) => {
-  res.send('Hello world');
-});
+loadContainer(app);
+
+app.use(loadControllers('./controllers/*.ts', { cwd: __dirname }));
 
 export { app };
